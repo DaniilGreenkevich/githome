05637567769f527bc72256857fb4d4507f89560a
DanilGorbunov <danilgorbunov@gmail.com>
2019-05-21 02:42:27 +0300

    module todo

---
+++ vue/vue-todo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1> labelnot app1 </h1>
+    <hr>
+    <TodoList
+        v-bind:todos="todos"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+export default {
+  name: 'app',
+  data() {
+    return {
+     todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 2, title: 'Купить пиво', completed: false},
+        {id: 3, title: 'Купить мясо', completed: false}
+     ]
+    }
+  },
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-todo/src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+        <li>
+             <span v-bind:class="{done: todo.completed}">
+                <input type='checkbox'
+                v-on:change="todo.completed = !todo.completed">
+                <strong>{{ todo.id }} </strong>
+                 {{ todo.title }}
+             </span>
+             <button class="rm"
+                v-on:click="$emit('remove-todo', todo.id)"
+             >&times;</button>
+        </li>
+</template>
+
+<script>
+ export default {
+   props: {
+    todo: {
+     type: Object,
+     requited: true
+    }
+   }
+ }
+</script>
+
+<style scoped>
+    li {
+        border: 1px solid #c4c4c4;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
+
+    .rm {
+        background: red;
+        border-radius: 50%;
+        color: #fff;
+        font-weight: bold;
+    }
+    .done {
+        text-decoration: line-through;
+    }
+    input {
+        margin-right: 1rem;
+    }
+
+</style>
